ea113f8d805b1f7feed7175031c5838a267e0b45
Mario Jose Ramos Mejia <marioramosmejia2243@gmail.com>
2023-05-27 00:34:36 -0600

    Dejando todo como estaba, para no fregar el deply en  githubPages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
 import { ContactComponent } from './shared/pages/contact-page/contact.component';
-import { ByCapitalPageComponent } from './countries/pages/by-capital-page/by-capital-page.component';
 
 const routes: Routes =[
     {
@@ -19,7 +18,7 @@ const routes: Routes =[
     },
     {
         path:'**',
-        component: ByCapitalPageComponent
+        redirectTo:'countries'
     }
 ];
 
