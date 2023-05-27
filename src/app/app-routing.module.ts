09e39e99498050c217fdb113c86b10a824b4ccbf
Mario Jose Ramos Mejia <marioramosmejia2243@gmail.com>
2023-05-27 00:30:39 -0600

    Probando cambios con el path **

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
 import { ContactComponent } from './shared/pages/contact-page/contact.component';
+import { ByCapitalPageComponent } from './countries/pages/by-capital-page/by-capital-page.component';
 
 const routes: Routes =[
     {
@@ -18,7 +19,7 @@ const routes: Routes =[
     },
     {
         path:'**',
-        redirectTo:'countries'
+        component: ByCapitalPageComponent
     }
 ];
 
